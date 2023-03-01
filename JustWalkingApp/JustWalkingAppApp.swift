b3f39fa4826ce5bc64c16f12f3a77c6ed4dc19f9
Tim Mitra <tim.mitra@spothero.com>
2023-03-01 15:45:57 -0500

    Initial Commit

---
+++ JustWalkingApp/JustWalkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWalkingAppApp.swift
+//  JustWalkingApp
+//
+//  Created by Tim Mitra on 2023-03-01.
+//
+
+import SwiftUI
+
+@main
+struct JustWalkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
