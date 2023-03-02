3d3ea03a31a57c0796216f114457c6235a0debd7
Tim Mitra <tim.mitra@spothero.com>
2023-03-01 23:34:45 -0500

    fixed widget?

--- JustWalking/JustWalking.swift
+++ JustWalking/JustWalking.swift
@@ -16,29 +16,48 @@ struct StepEntry: TimelineEntry {
 struct Provider: TimelineProvider {
     
     typealias Entry = StepEntry
-
+    
     func placeholder(in context: Context) -> StepEntry {
-        return StepEntry
+        return StepEntry(steps: stepCount)
     }
     
     @AppStorage("stepCount", store: UserDefaults(suiteName: "group.com.it-guy.justwalking"))
     var stepCount: Int = 0
-    
+
     func getSnapshot(in context: Context, completion: @escaping (Entry) -> ()) {
         let entry = StepEntry(steps: stepCount)
         completion(entry)
     }
-    
+
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         let entry = StepEntry(steps: stepCount)
         completion(Timeline(entries: [entry], policy: .never))
     }
 }
 
-struct StepView: View {
-    let entry: Provider.Entry
-    
+struct JustWalkingEntryView : View {
+    var entry: Provider.Entry
+
     var body: some View {
         Text("\(entry.steps)")
+    }
+}
+
+struct JustWalking: Widget {
+    let kind: String = "JustWalking"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            JustWalkingEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct JustWalking_Previews: PreviewProvider {
+    static var previews: some View {
+        JustWalkingEntryView(entry: StepEntry(steps: 237))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
